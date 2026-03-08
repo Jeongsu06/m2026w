9bdfc5c220c94320c446047691ce7ea2cc97ca9a
kjs06 <kjs06@jgim>
2026-03-08 11:41:01 +0900

    update files

---
+++ m00/HelloWorld.m
@@ -0,0 +1,29 @@
+%[text] # 맨 처음 해보는 MATLAB
+%[text] by Jeongsu Kim
+%[text] ## 마방진 만들기
+%[text] 
+M = magic(4) %[output:25560ef1]
+%%
+
+%[text] ## 인사하기
+disp("HELLO,World!") %[output:48636b90]
+%%
+%[text] ## 100까지 더하기
+d=1:100;
+s=sum(d) %[output:496575be]
+%[text] 
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"inline"}
+%---
+%[output:25560ef1]
+%   data: {"dataType":"matrix","outputData":{"columns":4,"name":"M","rows":4,"type":"double","value":[["16","2","3","13"],["5","11","10","8"],["9","7","6","12"],["4","14","15","1"]]}}
+%---
+%[output:48636b90]
+%   data: {"dataType":"text","outputData":{"text":"HELLO,World!\n","truncated":false}}
+%---
+%[output:496575be]
+%   data: {"dataType":"textualVariable","outputData":{"name":"s","value":"5050"}}
+%---
